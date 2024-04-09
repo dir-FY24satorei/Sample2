132f967900e498cb6af25d114db17061f6347243
dir-24satorei <rei.sato.v322@dirbato.co.jp>
2024-04-09 12:59:47 +0900

    最初のコミット

---
+++ Sample2/src/sample/Variable.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var number1 = 1;
+        System.out.println(number1);
+        
+        var secondsPerDay = 86400;
+        System.out.println(secondsPerDay); 
+
+    }
+
+}
